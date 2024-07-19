1eedbb8fc649bdbd07de4b555edc8d9a2d83f4b9
Rabbit025879 <rabbit025879@gmail.com>
2024-07-19 16:57:37 +0800

    modified:   Core/Inc/TIM_IT.h 	modified:   Core/Inc/path.h 	new file:   Core/Inc/path_vector_def.h 	modified:   Core/Src/TIM_IT.cpp 	modified:   Core/Src/local_planner.cpp 	modified:   Core/Src/path.cpp Fix: vector transfer

--- Core/Inc/path.h
+++ Core/Inc/path.h
@@ -12,15 +12,9 @@
 #ifndef INC_PATH_H_
 #define INC_PATH_H_
 
+#include "path_vector_def.h"
 #include "TIM_IT.h"
 #include "local_planner.h"
-#include <vector>
-
-struct point{
-	double x = 0.0;
-	double y = 0.0;
-	double w = 0.0;
-};
 
 extern int done;
 
